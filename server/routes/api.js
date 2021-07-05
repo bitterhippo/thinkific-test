c4951ccab491dabf48a2630fc1f4d2441fcff696
Noah <bitterhippo@gmail.com>
2021-07-06 01:54:18 +0900

    plusOne route name > next
    this conforms to the documentation provided with this challenge

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const apiCtrl = require('../controllers/api');
 
 router.post('/current', apiCtrl.current);
-router.post('/plusOne', apiCtrl.plusOne);
+router.post('/next', apiCtrl.next);
 router.post('/set', apiCtrl.set);
 
 module.exports = router;
